255987f70bb8c59674bf08deb53a736bd865209e
robvanderleek <robvanderleek@gmail.com>
2013-12-22 21:58:47 +0100

    Adapted for removal of jar2sh.

--- install.sh
+++ install.sh
@@ -2,7 +2,13 @@
 TARGET_DIR="$HOME/Desktop"
 mvn clean compile assembly:single
 cp target/*with-dependencies.jar $TARGET_DIR/jlifx.jar
-./jar2sh "$TARGET_DIR/jlifx" $TARGET_DIR/jlifx.jar
 echo "Launchable jar (jlifx.jar) copied to $TARGET_DIR"
-echo "Executable shell script (jlifx) copied to $TARGET_DIR"
+
+which jar2sh > /dev/null
+if [ "$?" == "0" ]
+then
+	jar2sh $TARGET_DIR/jlifx $TARGET_DIR/jlifx.jar
+	echo "Executable shell script (jlifx) copied to $TARGET_DIR"
+fi
+
 echo "Installation done."
