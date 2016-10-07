30016621fa004ba225b3ba0657d31c716333e846
Rob van der Leek <r.vanderleek@sig.eu>
2016-10-07 19:31:02 +0200

    Migrated to Mockito, a breath of fresh air

--- src/test/java/jlifx/commandline/command/RainbowCommandTest.java
+++ src/test/java/jlifx/commandline/command/RainbowCommandTest.java
@@ -1,27 +1,26 @@
 package jlifx.commandline.command;
 
-import static org.easymock.EasyMock.isA;
+import static org.mockito.AdditionalMatchers.gt;
+import static org.mockito.Mockito.atLeastOnce;
+import static org.mockito.Mockito.verify;
 
 import java.awt.Color;
 
+import org.junit.Test;
+import org.mockito.Mockito;
+
 import jlifx.bulb.AbstractJLifxTestCase;
 import jlifx.bulb.IBulb;
-
-import org.easymock.EasyMock;
-import org.junit.Test;
 
 public class RainbowCommandTest extends AbstractJLifxTestCase {
 
     @Test
     public void testTimed() throws Exception {
         IBulb bulb = getMockedBulb();
-        bulb.colorize(isA(Color.class), EasyMock.gt(0), EasyMock.eq(1.0F));
-        EasyMock.expectLastCall().atLeastOnce();
-        replayAll();
         
         executeCommand(new RainbowCommand(), bulb, "duration", "2");
 
-        verifyAll();
+        verify(bulb, atLeastOnce()).colorize(Mockito.isA(Color.class), gt(0), Mockito.eq(1.0F));
     }
 
 }
