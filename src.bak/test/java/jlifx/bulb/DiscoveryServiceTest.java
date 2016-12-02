cc3d05477c00107649b3631f16dbe1b85eeceefa
Rob van der Leek <r.vanderleek@sig.eu>
2016-12-02 13:15:56 +0100

    Copy & Paste

---
+++ src.bak/test/java/jlifx/bulb/DiscoveryServiceTest.java
@@ -0,0 +1,74 @@
+package jlifx.bulb;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.util.List;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.channel.socket.DatagramPacket;
+import jlifx.boblightd.NetworkUtils;
+import jlifx.packet.StatusResponsePacket;
+import jlifx.packet.StatusResponsePacketTest;
+
+public class DiscoveryServiceTest extends AbstractJLifxTestCase {
+    private int testDiscoveryPort;
+
+    @Before
+    public void setup() {
+        testDiscoveryPort = getFreeLocalPort();
+        DiscoveryService.setGatewayDiscoveryPort(testDiscoveryPort);
+        DiscoveryService.setIgnoreGatewaysOnLocalhost(false);
+    }
+
+    @After
+    public void tearDown() {
+        DiscoveryService.setGatewayDiscoveryPort(56700);
+        DiscoveryService.setIgnoreGatewaysOnLocalhost(true);
+    }
+
+    private static class TestGatewayBulbChannelHandlerAdapter extends SimpleChannelInboundHandler<DatagramPacket> {
+
+        @Override
+        protected void messageReceived(ChannelHandlerContext ctx, DatagramPacket msg) throws Exception {
+            DatagramSocket socket = new DatagramSocket();
+            StatusResponsePacket packet = StatusResponsePacketTest.makeTestPacket();
+            packet.setGatewayMac(TEST_MAC_ADDRESS_1);
+            packet.setTargetMac(TEST_MAC_ADDRESS_1);
+            socket.send(packet.toDatagramPacket(msg.sender().getAddress()));
+            socket.close();
+        }
+
+    }
+
+    @Test
+    public void testDiscoverGatewayBulb() throws Exception {
+        ChannelFuture channelFuture = NetworkUtils.startUdpServer(testDiscoveryPort,
+            new TestGatewayBulbChannelHandlerAdapter());
+
+        GatewayBulb result = DiscoveryService.discoverGatewayBulb();
+
+        assertNotNull(result);
+        assertArrayEquals(TEST_MAC_ADDRESS_1, result.getGatewayMacAddress());
+
+        channelFuture.channel().close().sync();
+    }
+
+    @Test
+    public void testGetNetworkBroadcastAddresses() throws Exception {
+        List<InetAddress> result = DiscoveryService.getNetworkBroadcastAddresses();
+
+        assertNotNull(result);
+        assertTrue(result.size() > 0);
+    }
+
+}
