673888efbc22040dbee73863627d3904a7f26b61
Rob van der Leek <r.vanderleek@sig.eu>
2016-12-02 13:15:56 +0100

    Copy & Paste

---
+++ src.bak/main/java/jlifx/bulb/BulbMeshFirmwareStatus.java
@@ -0,0 +1,38 @@
+package jlifx.bulb;
+
+import java.util.Date;
+
+import jlifx.commandline.Utils;
+import jlifx.packet.Packet;
+
+public class BulbMeshFirmwareStatus {
+    private final Date buildTimestamp;
+    private final Date installTimestamp;
+    private final int version;
+
+    public BulbMeshFirmwareStatus(Date buildTimestamp, Date installTimestamp, int version) {
+        this.buildTimestamp = buildTimestamp;
+        this.installTimestamp = installTimestamp;
+        this.version = version;
+    }
+
+    public Date getBuildTimestamp() {
+        return buildTimestamp;
+    }
+
+    public Date getInstallTimestamp() {
+        return installTimestamp;
+    }
+
+    public int getVersion() {
+        return version;
+    }
+
+    public static BulbMeshFirmwareStatus fromPacket(Packet packet) {
+        byte[] payload = packet.getPayload();
+        // 8 byte first date
+        // 8 byte second date
+        int version = Utils.from32bitsLittleEndian(payload[16], payload[17], payload[18], payload[19]);
+        return new BulbMeshFirmwareStatus(new Date(), new Date(), version);
+    }
+}
