e5927de0cb8643e1a51f4428cfbcbeab3e2612dc
Rob van der Leek <r.vanderleek@sig.eu>
2016-12-02 13:15:56 +0100

    Copy & Paste

---
+++ src.bak/main/java/jlifx/commandline/command/BlinkCommand.java
@@ -0,0 +1,39 @@
+package jlifx.commandline.command;
+
+import java.io.IOException;
+import java.io.PrintStream;
+import java.util.Collection;
+
+import jlifx.bulb.IBulb;
+import jlifx.commandline.AbstractBulbCommand;
+
+public class BlinkCommand extends AbstractBulbCommand {
+
+    @Override
+    public boolean execute(Collection<IBulb> bulbs, String[] commandArgs, PrintStream out) throws Exception {
+        if (commandArgs.length == 2) {
+            int times = Integer.parseInt(commandArgs[1]);
+            for (int i = 0; i < times; i++) {
+                blinkBulbs(bulbs);
+            }
+        } else {
+            startKeyListenerThread(out);
+            while (!isInterrupted()) {
+                blinkBulbs(bulbs);
+            }
+        }
+        return true;
+    }
+
+    private void blinkBulbs(Collection<IBulb> bulbs) throws IOException, InterruptedException {
+        for (IBulb bulb : bulbs) {
+            bulb.switchOff();
+        }
+        Thread.sleep(500);
+        for (IBulb bulb : bulbs) {
+            bulb.switchOn();
+        }
+        Thread.sleep(500);
+    }
+
+}
