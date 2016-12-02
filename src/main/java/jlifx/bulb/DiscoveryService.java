21c6588b3dfbbfbcc5aa24391e03f952bba5476d
Rob van der Leek <r.vanderleek@sig.eu>
2016-12-02 13:36:03 +0100

    Quick hack

--- src/main/java/jlifx/bulb/DiscoveryService.java
+++ src/main/java/jlifx/bulb/DiscoveryService.java
@@ -58,6 +58,66 @@ public final class DiscoveryService {
             result = waitForReply(socket);
             retries--;
         }
+        int retries = 3;
+        while (result == null && retries > 0) {
+            socket.send(datagramPacket);
+            result = waitForReply(socket);
+            retries--;
+        }
+        int retries = 3;
+        while (result == null && retries > 0) {
+            socket.send(datagramPacket);
+            result = waitForReply(socket);
+            retries--;
+        }
+        int retries = 3;
+        while (result == null && retries > 0) {
+            socket.send(datagramPacket);
+            result = waitForReply(socket);
+            retries--;
+        }
+        int retries = 3;
+        while (result == null && retries > 0) {
+            socket.send(datagramPacket);
+            result = waitForReply(socket);
+            retries--;
+        }
+        int retries = 3;
+        while (result == null && retries > 0) {
+            socket.send(datagramPacket);
+            result = waitForReply(socket);
+            retries--;
+        }
+        int retries = 3;
+        while (result == null && retries > 0) {
+            socket.send(datagramPacket);
+            result = waitForReply(socket);
+            retries--;
+        }
+        int retries = 3;
+        while (result == null && retries > 0) {
+            socket.send(datagramPacket);
+            result = waitForReply(socket);
+            retries--;
+        }
+        int retries = 3;
+        while (result == null && retries > 0) {
+            socket.send(datagramPacket);
+            result = waitForReply(socket);
+            retries--;
+        }
+        int retries = 3;
+        while (result == null && retries > 0) {
+            socket.send(datagramPacket);
+            result = waitForReply(socket);
+            retries--;
+        }
+        int retries = 3;
+        while (result == null && retries > 0) {
+            socket.send(datagramPacket);
+            result = waitForReply(socket);
+            retries--;
+        }
         socket.close();
         return result;
     }
