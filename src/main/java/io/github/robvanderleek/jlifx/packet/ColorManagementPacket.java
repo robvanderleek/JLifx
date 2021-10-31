aa71ede71c7f0f8ca85a6d0ba51e5d85189cf1f3
Luke Anderson <stuntguy3000@gmail.com>
2021-11-01 00:52:53 +1030

    Fix: ColorManagementPacket Duration is not correctly converted to Little Endian Bytes (#172)

--- src/main/java/io/github/robvanderleek/jlifx/packet/ColorManagementPacket.java
+++ src/main/java/io/github/robvanderleek/jlifx/packet/ColorManagementPacket.java
@@ -14,13 +14,14 @@ class ColorManagementPacket extends Packet {
         byte[] saturationBytes = floatToBytes(hsbValues[1]);
         byte[] brightnessBytes = floatToBytes(brightness);
         byte[] kelvin = new byte[]{0x0a, (byte) 0xf0};
+        byte[] duration = intToBytes(fadetime);
         byte[] payload = new byte[]{streaming, //
                 hueBytes[1], hueBytes[0], //
                 saturationBytes[1], saturationBytes[0], //
                 brightnessBytes[1], brightnessBytes[0], //
                 kelvin[1], //
                 kelvin[0], //
-                0x00, (byte) fadetime, 0x00, 0x00};
+                duration[3], duration[2], duration[1], duration[0]};
         setPayload(payload);
     }
 
